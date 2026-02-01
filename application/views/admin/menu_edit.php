07048a82f91b8b249ae63a38f0e76892b039b401
MVC Developer <dev@mvc.local>
2026-02-02 03:14:20 +0900

    fix: CKEditor 이미지 업로드 어댑터 추가
    - menu_edit.php에 Custom Upload Adapter 추가
    - simpleUpload 제거 (작동하지 않음)
    - FileRepository.createUploadAdapter 구현
    - 업로드 URL: /upload/page/image
    - 진행률 표시 기능 추가
    
    문제 해결:
    - filerepository-no-upload-adapter 오류 해결
    - 이미지 드래그 앤 드롭 업로드 지원
    - 이미지 붙여넣기 업로드 지원

--- application/views/admin/menu_edit.php
+++ application/views/admin/menu_edit.php
@@ -351,6 +351,75 @@ function deletePageFile(fileId) {
 // CKEditor 초기화 (지연 로드)
 let editorpage_content = null;
 
+// Custom Upload Adapter
+class CustomUploadAdapter {
+    constructor(loader) {
+        this.loader = loader;
+    }
+
+    upload() {
+        return this.loader.file
+            .then(file => new Promise((resolve, reject) => {
+                this._initRequest();
+                this._initListeners(resolve, reject, file);
+                this._sendRequest(file);
+            }));
+    }
+
+    abort() {
+        if (this.xhr) {
+            this.xhr.abort();
+        }
+    }
+
+    _initRequest() {
+        const xhr = this.xhr = new XMLHttpRequest();
+        xhr.open('POST', '/upload/page/image', true);
+        xhr.responseType = 'json';
+    }
+
+    _initListeners(resolve, reject, file) {
+        const xhr = this.xhr;
+        const loader = this.loader;
+        const genericErrorText = `파일을 업로드할 수 없습니다: ${file.name}.`;
+
+        xhr.addEventListener('error', () => reject(genericErrorText));
+        xhr.addEventListener('abort', () => reject());
+        xhr.addEventListener('load', () => {
+            const response = xhr.response;
+
+            if (!response || response.error) {
+                return reject(response && response.error ? response.error.message : genericErrorText);
+            }
+
+            resolve({
+                default: response.url
+            });
+        });
+
+        if (xhr.upload) {
+            xhr.upload.addEventListener('progress', evt => {
+                if (evt.lengthComputable) {
+                    loader.uploadTotal = evt.total;
+                    loader.uploaded = evt.loaded;
+                }
+            });
+        }
+    }
+
+    _sendRequest(file) {
+        const data = new FormData();
+        data.append('upload', file);
+        this.xhr.send(data);
+    }
+}
+
+function CustomUploadAdapterPlugin(editor) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+        return new CustomUploadAdapter(loader);
+    };
+}
+
 function initPageContentEditor() {
     if (editorpage_content) {
         return; // 이미 초기화됨
@@ -396,9 +465,7 @@ function initPageContentEditor() {
                     'linkImage'
                 ]
             },
-            simpleUpload: {
-                uploadUrl: '/upload/page/image'
-            },
+            extraPlugins: [CustomUploadAdapterPlugin],
             licenseKey: 'GPL'
         })
         .then(editor => {
